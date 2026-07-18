322968da49499bb1147b575708785fd0cb86ef27
andyrbek2709-tech <andyrbek2709@gmail.com>
2026-07-19 02:04:13 +0500

    assets: export photorealistic burger layer set

---
+++ src/assets/burger-real/index.ts
@@ -0,0 +1,21 @@
+import bottom from './bottom'
+import sauce from './sauce'
+import lettuce from './lettuce'
+import patty from './patty'
+import cheese from './cheese'
+import pickles from './pickles'
+import tomato from './tomato'
+import onion from './onion'
+import top from './top'
+
+export const burgerReal = {
+  bottom,
+  sauce,
+  lettuce,
+  patty,
+  cheese,
+  pickles,
+  tomato,
+  onion,
+  top,
+}
