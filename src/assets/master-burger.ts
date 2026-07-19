e29852eee14d46f5fe48d07730d406089b98f944
andyrbek2709-tech <andyrbek2709@gmail.com>
2026-07-19 23:26:41 +0500

    assets: assemble master burger data image

---
+++ src/assets/master-burger.ts
@@ -0,0 +1,10 @@
+import part0 from './master-burger/part0'
+import part1 from './master-burger/part1'
+import part2 from './master-burger/part2'
+import part3 from './master-burger/part3'
+import part4 from './master-burger/part4'
+import part5 from './master-burger/part5'
+import part6 from './master-burger/part6'
+import part7 from './master-burger/part7'
+
+export const masterBurger = `data:image/webp;base64,${part0}${part1}${part2}${part3}${part4}${part5}${part6}${part7}`
